431bb5e2575a75d9295a2ecaafdf37817b2d7844
heuman-a <heuman-a>
2025-09-04 21:03:05 +0200

    Fix Timezone for German DST

--- src/utcOffset.h
+++ src/utcOffset.h
@@ -1,7 +1,14 @@
 #ifndef WORDCLOCK_UTC_OFFSET_H
 #define WORDCLOCK_UTC_OFFSET_H
+#include <Timezone.h>
 
 class UtcOffset {
+  private:
+
+    static TimeChangeRule CEST;     // Central European Summer Time
+    static TimeChangeRule CET;       // Central European Standard Time
+    static Timezone CE;
+
   public:
     static int getLocalizedUtcOffset();
 };
